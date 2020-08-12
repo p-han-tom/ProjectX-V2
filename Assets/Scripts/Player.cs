6f718449b3802dd78600d3fd4c6cc37dcf4919e0
p-han-tom <hantom0127@gmail.com>
2020-08-12 16:41:07 -0400

    Movement and .gitignore

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Movement
+    Vector2 movement;
+    float movementSpeed = 5f;
+
+    // Components
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void CheckInput() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void Move() {
+        rb.velocity = movement * movementSpeed;
+    }
+
+    void Update()
+    {
+        CheckInput();
+        Move();
+    }
+}
