14964f73bf47a8726b584981666999569f54818a
JerryLin1 <jerreelynn@gmail.com>
2020-08-12 17:58:33 -0400

    abilities i guess

---
+++ Assets/Scripts/ItemEffects/ActiveAbilities/ActiveAbility.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActiveAbility : ItemEffect
+{
+    protected float cooldown;
+    void Start()
+    {
+        StartAbility();
+    }
+    public abstract void Cast();
+    protected abstract void StartAbility();
+}
---
+++ Assets/Scripts/ItemEffects/ActiveAbilities/SwordSlash.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSlash : ActiveAbility
+{
+    protected override void StartAbility() {
+        name = "SwordSlash";
+        cooldown = 0f;
+    }
+    public override void Cast() {
+        Debug.Log("SLASHED!");
+    }
+}
