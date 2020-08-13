4f4a887801e0e13e93c0ced95c9ae9b332ccd6ad
JerryLin1 <jerreelynn@gmail.com>
2020-08-13 15:37:33 -0400

    joe

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    Transform player;
-    // Start is called before the first frame update
-    void Start()
+    // Update is called once per frame
+    Transform leader;
+    Vector3 leaderVelocity;
+    Vector3 mousePos;
+    Vector3 leaderPos;
+    float xMaxDist = 1f;
+    float yMaxDist = 1f;
+    float cameraTime = 0.1f;
+    float maxCameraSpeed = 100f;
+    void Start() {
+        leader = GameObject.Find("Player").transform;
+    }
+    void FixedUpdate()
     {
-        player = GameObject.Find("Player").transform;
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        leaderPos = leader.position;
+        leaderVelocity = leader.GetComponent<Rigidbody2D>().velocity;
+        float x = (leaderPos.x + mousePos.x) / 2;
+        if (x > leaderPos.x + xMaxDist) x = leaderPos.x + xMaxDist;
+        else if (x < leaderPos.x - xMaxDist) x = leaderPos.x - xMaxDist;
+        float y = (leaderPos.y + mousePos.y) / 2;
+        if (y > leaderPos.y + yMaxDist) y = leaderPos.y + yMaxDist;
+        else if (y < leaderPos.y - yMaxDist) y = leaderPos.y - yMaxDist;
+        Vector3 center = new Vector3(x, y, transform.position.z);
+        if (leader != null)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, center, ref leaderVelocity, cameraTime, maxCameraSpeed);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setTarget(Transform target)
     {
-        Camera.main.transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        leader = target;
+    }
+    public IEnumerator cameraShake(float duration, float magnitude)
+    {
+        float timeElapsed = 0f;
+        while (timeElapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
     }
 }
